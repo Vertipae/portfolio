510757d99194535e30e629eead57c29e5415a9ba
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-08-06 18:03:43 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -35,3 +35,22 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// Modal project
+var images = document.querySelectorAll(".myImg");
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+images.forEach(img => {
+  img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  };
+});
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+};
