75f6d826aa99386dca003fc4706e7649948adbac
Vertipae <33002993+Vertipae@users.noreply.github.com>
2020-07-13 16:41:01 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,56 +1,57 @@
 // Select DOM items
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-item"); // Similar to an array
+const menuBtn = document.querySelector(".menu-btn")
+const menu = document.querySelector(".menu")
+const menuNav = document.querySelector(".menu-nav")
+const menuBranding = document.querySelector(".menu-branding")
+const navItems = document.querySelectorAll(".nav-item") // Similar to an array
 
 // Set initial state of menu
-let showMenu = false;
+let showMenu = false
 
-menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener("click", toggleMenu)
 
 function toggleMenu() {
   if (!showMenu) {
     // If menu is not shown then show the menu. !showMenu <=> showMenu == false
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show")); // It will loop through items and then add show class to each item
+    menuBtn.classList.add("close")
+    menu.classList.add("show")
+    menuNav.classList.add("show")
+    menuBranding.classList.add("show")
+    navItems.forEach((item) => item.classList.add("show")) // It will loop through items and then add show class to each item
 
     // Set menu state
     // Now menu should be open
-    showMenu = true;
+    showMenu = true
   } else {
     // Hide the menu
     // When we click the menu it should remove all these classes
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show")); // It will loop through items and then remove show class from each item
+    menuBtn.classList.remove("close")
+    menu.classList.remove("show")
+    menuNav.classList.remove("show")
+    menuBranding.classList.remove("show")
+    navItems.forEach((item) => item.classList.remove("show")) // It will loop through items and then remove show class from each item
 
     // Set menu state
-    showMenu = false;
+    showMenu = false
   }
 }
 
 // Modal project
-var images = document.querySelectorAll(".myImg");
-var modal = document.getElementById("myModal");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-images.forEach(img => {
-  img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  };
-});
+var images = document.querySelectorAll(".myImg")
+var modal = document.getElementById("myModal")
+var modalImg = document.getElementById("img01")
+var captionText = document.getElementById("caption")
+images.forEach((img) => {
+  img.onclick = function () {
+    modal.style.display = "block"
+    modalImg.src = this.src
+    captionText.innerHTML = this.alt
+  }
+})
 
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function() {
-  modal.style.display = "none";
-};
+var span = document.getElementsByClassName("close")[0]
+if (span) {
+  span.onclick = function () {
+    modal.style.display = "none"
+  }
+}
